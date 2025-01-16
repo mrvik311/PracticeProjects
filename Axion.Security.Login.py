acb11a700b4f9f8ea84852a91ef507717c391f70
mrvik311 <mrvik311@gmail.com>
2025-01-16 18:43:14 -0500

    Add files via upload

---
+++ Axion.Security.Login.py
@@ -0,0 +1,24 @@
+import os
+import time
+
+def clear_console():
+  """Clears the console output."""
+  if os.name == 'nt': 
+    os.system('cls')  # For Windows
+  else:
+    os.system('clear')  # For macOS and Linux
+
+print("""Axion Security services loading...""")
+time.sleep(3)
+print("Opening servers for login...")
+time.sleep(5)
+clear_console()  # Call clear_console() here
+
+passcode = input("Input passcode:")
+if passcode == "00.2008.00":
+  print("Hello Aliyan Ahmed. Welcome to the Axion Security Servers database. ")
+else:
+  print("Access Denied and system locked.")
+  time.sleep(3)
+  clear_console()
+
